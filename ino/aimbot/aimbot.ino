bb74dfff4862dfa5e4cf023aee0fc766682f59e3
DjDalyDalito <161302732+DjDalyDalito@users.noreply.github.com>
2025-04-24 20:17:36 +0200

    1
    1

--- ino/aimbot/aimbot.ino
+++ ino/aimbot/aimbot.ino
@@ -1,16 +1,17 @@
-#include <hidboot.h>
+/*#include <hidboot.h>
 #include <usbhub.h>
-#include <Wire.h>
-#include <mouse.h>
+#include <Wire.h>*/
+#include <Mouse.h>
 
-USB Usb; /*Skapar ett usb objekt */
-USBHub Hub(&Usb); /*Skapar ett usb-hub objekt med referens och utgångspunkt till usb objektet */
-HIDBoot < USB_HID_PROTOCOL_KEYBOARD | USB_HID_PROTOCOL_MOUSE Hid> HidKBM(&Usb); /* Ett HIDBoot-objekt som hanterar kommunikation med USB-HID-enheter i det här fallet tangentbordet and musen*/
-HIDBoot < USB_HID_PROTOCOL_MOUSE Hid> HidMouse(&Usb); /*Ett HIDBoot-obhejt som enbart hanterar kommunikation med musen */
+/*USB Usb; /*Skapar ett usb objekt */
+/*USBHub Hub(&Usb); /*Skapar ett usb-hub objekt med referens och utgångspunkt till usb objektet */
+/*HIDBoot < USB_HID_PROTOCOL_KEYBOARD | USB_HID_PROTOCOL_MOUSE Hid> HidKBM(&Usb); /* Ett HIDBoot-objekt som hanterar kommunikation med USB-HID-enheter i det här fallet tangentbordet and musen*/
+/*HIDBoot < USB_HID_PROTOCOL_MOUSE Hid> HidMouse(&Usb); /*Ett HIDBoot-obhejt som enbart hanterar kommunikation med musen */
 
-int neg = -127;
+int neg = -127; /*Mouse.move supportar bara 128px per funktionsanrop*/
 int pos = 127;
 
+/*
 int lmb;
 int rmb;
 int mmb;
@@ -27,7 +28,6 @@ class mousereport : public mousereporter {
 
 void mousereporter::OnMouseMove (mouseinfo)
 {
-  /*SÄTT MOUSE MOVEMENTS*/
   Mouse.move(1, 1, 0);
 }
 
@@ -59,17 +59,39 @@ void mousereporter::OnMiddleButtonUp (mouseinfo)
 void mousereporter::OnMiddleButtonDown (mouseinfo)
 {
   mmb = 1;
-}
+}*/
 
 void setup() {
+  Serial.begin(9600); /*Använd 151200 dvs 1 ms senare i projektet när den faktiskt är användbar*/
+  while (!Serial); /*Väntar tills datorn faktiskt har öppnat porten*/
   Mouse.begin();
-  Serial.begin(9600);
-  Usb.Init();
-  HidKBM. 
-  HidMouse. 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  static char buffer[16]; /*buffer som vi samlar inputen i och skickar till processInput */
+  static int index = 0; /*räknare som visar vart i bufferten nästa tecken ska hamna*/
+  while (Serial.available()){ /*så länge det finns minst ett tecken att läsa från datorn*/
+    char c = Serial.read(); /*plocka ut tecknet och lägg i c*/
+    if (c == '\n'){ /*om det är slutet på raden*/
+      buffer[index] = '\0'; /*gör om rådatan till sträng*/
+      processInput(buffer); /*passerar in buffern i funktionen som utför draget*/
+      index = 0; /*börja om buffern för nästa rad*/
+    }
+    else if (index < sizeof(buffer) - 1) { /*om buffern inte är full*/
+      buffer[index] = c; /*lägg c på plats index*/
+      index += 1; /*öka index*/
+    }
+  }
+}
 
+void processInput(char *line) {
+  int x = 0, y = 0;
+  if (sscanf(line, "%d %d", &x, &y) == 2) { /*taget från google läser av texten och kollar om den hittar 2 heltal som motsvara y och x värderna */
+    x = constrain(x, neg, pos); /*constrainar talet till max 127 minst -127*/
+    y = constrain(y, neg, pos);
+    Mouse.move(x,y,0); /*inget på z-axeln*/
+    Serial.println("OK");
+  } else {
+    Serial.println("ERR");
+  }
 }
